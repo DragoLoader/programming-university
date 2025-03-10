8070379ad2afc857e4946c71dac7086711de3588
DragoLoader <dragoyoon@gmail.com>
2025-03-10 17:06:12 +0900

    첫번째 commit
    java 연습 클래스 3개 생성

---
+++ project1/src/code01.java
@@ -0,0 +1,5 @@
+public class code01 {
+    public static void main(String[] args) {
+      System.out.println("궯둙셇롌");
+    }
+}
---
+++ project1/src/code02.java
@@ -0,0 +1,6 @@
+public class code02 {
+    public static void main(String[] args) {
+        System.out.println("AA");
+        System.out.println("고라니");
+    }
+}
