f76689ef991abdc95dd59d83f1914ace3b12fd2c
DragoLoader <dragoyoon@gmail.com>
2025-03-11 09:57:18 +0900

    제출2
    변수 배움 ㅇㅇ

---
+++ project2/src/code04.java
@@ -0,0 +1,17 @@
+public class code04 {
+    public static void main (String[] args) {
+        int number;
+        number = 90;
+        int percentA = 10, percentB = 20, percentC = 30;
+        char grade = 'A';
+        double average;
+        average = 89.5;
+        average = 75.8;
+        char name = 75;
+        int numberNext = number;
+        System.out.println(numberNext);
+        System.out.println("바나나: " + numberNext);
+        System.out.println(name);
+    }
+
+}
